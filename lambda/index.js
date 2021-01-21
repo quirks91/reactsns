c4ce2a9bff3870468ff2bac5b74fa861486cd199
yeonghoon <wolfhoons@gmail.com>
2021-01-22 00:57:40 +0900

    fix lambda

--- lambda/index.js
+++ lambda/index.js
@@ -1,15 +1,14 @@
-//aws자체에서 인증을 확인해서 따로 인증을 해줄 필요 없다
 const AWS = require('aws-sdk');
 const sharp = require('sharp');
 
 const s3 = new AWS.S3();
 
 exports.handler = async (event, context, callback) => {
-  const Bucket = event.Records[0].s3.bucket.name; // reactsnsimage-s3
-  const Key = decodeURIComponent(event.Recoreds[0].s3.object.key); // 한글 이름 파일
+  const Bucket = event.Records[0].s3.bucket.name;
+  const Key = decodeURIComponent(event.Records[0].s3.object.key);
   console.log(Bucket, Key);
   const filename = Key.split('/')[Key.split('/').length - 1];
-  const ext = Key.split('.')[Key.split('.').length -1].toLowerCase(); // 확장자 문자열을 소문자로 전환
+  const ext = Key.split('.')[Key.split('.').length - 1].toLowerCase();
   const requiredFormat = ext === 'jpg' ? 'jpeg' : ext;
   console.log('filename', filename, 'ext', ext);
 
@@ -17,10 +16,9 @@ exports.handler = async (event, context, callback) => {
     const s3Object = await s3.getObject({ Bucket, Key }).promise();
     console.log('original', s3Object.Body.length);
     const resizedImage = await sharp(s3Object.Body)
-    .resize(400, 400, { fit: 'inside' })
-    .toFormat(requiredFormat)
-    .toBuffer();
-
+      .resize(400, 400, { fit: 'inside' })
+      .toFormat(requiredFormat)
+      .toBuffer();
     await s3.putObject({
       Bucket,
       Key: `thumb/${filename}`,
@@ -28,10 +26,8 @@ exports.handler = async (event, context, callback) => {
     }).promise();
     console.log('put', resizedImage.length);
     return callback(null, `thumb/${filename}`);
-
-  } catch(error) {
+  } catch (error) {
     console.error(error)
-    return callback(error); // 람다가 끝날 때 
+    return callback(error);
   }
 }
-
